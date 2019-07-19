261407fc950c5ce2481eeb955c21d8d1b6e3f88f
Rory Murray <rory.christian.murray@gmail.com>
2019-07-19 11:19:33 -0400

    initial commit

---
+++ sprint-form-management/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>App incoming</div>
+    </div>
+  );
+}
+
+export default App;
